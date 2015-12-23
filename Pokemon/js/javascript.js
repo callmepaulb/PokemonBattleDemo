e5890241320eb1e31fdfafe5e1d9cc825c8b0973
callmepaulb <paul.w.bie@gmail.com>
2015-12-22 21:22:37 -0800

    Version with only one move animation

---
+++ Pokemon/js/javascript.js
@@ -0,0 +1,181 @@
+var charmander = {
+    name: "Charmander",
+    health: 100,
+    lvl: 12,
+    effect: null,
+    moves: [{
+        name: "Ember",
+        type: "Attack",
+        power: 40,
+        accuracy: .80
+    },
+    {
+        name: "Scratch",
+        type: "Attack",
+        power: 30,
+        accuracy: .90
+    },
+    {
+        name: "Growl",
+        type: "Defense",
+        power: .20,
+        accuracy: 1.0
+    },
+    {
+        name: "Smokescreen",
+        type: "Defense",
+        power: .65,
+        accuracy: .90
+    }]
+    
+};
+
+var pikachu = {
+    name: "Pikachu",
+    health: 100,
+    lvl: 12,
+    effect: null,
+    moves: [{
+        name: "Thunder Shock",
+        type: "Attack",
+        power: 40,
+        accuracy: .80
+    },
+    {
+        name: "Tail Whip",
+        type: "Defense",
+        power: .75,
+        accuracy: .90
+    },
+    {
+        name: "Growl",
+        type: "Defense",
+        power: .20,
+        accuracy: 1.0
+    },
+    {
+        name: "Quick Attack",
+        type: "Attack",
+        power: 30,
+        accuracy: .90
+    }]
+    
+};
+
+var currentState;
+var cpuPokemon;
+var userPokemon;
+
+var cpuTurn = {
+    play: function() {
+        var randomMove = Math.floor(Math.random() * 4);
+        var currentCPUMove = cpuPokemon.moves[randomMove];
+        
+        var setUpCPUField = function () {
+            $("#chat-text").text("What will " + cpuPokemon.name + " do?");
+            prepareToAttack();
+        };
+        
+        var prepareToAttack = function () {
+            $("#pikachu-img").animate({
+                top: "-=25"
+            }, 150, function() {
+                $("#pikachu-img").animate({
+                    top: "+=25"
+                }, 150, function() {
+                    $("#pikachu-img").animate({
+                        top: "-=25"
+                    }, 150, function() {
+                        $("#pikachu-img").animate({
+                            top: "+=25"
+                        }, 150)
+                    });
+                })
+            });
+            getAccuracy();
+        };
+        
+        var getAccuracy = function() {
+            var setAccuracy = Math.random();
+            if (setAccuracy <= currentCPUMove.accuracy) {
+                $("#chat-text").text(cpuPokemon.name + " used " + currentCPUMove.name + "!");
+                getMoveType();
+            } else {
+                $("#chat-text").text(cpuPokemon.name + " used " + currentCPUMove.name + " but it missed!");
+                currentState = playerTurn;
+                setTimeout(loop, 1500);
+            }
+        };
+        
+        var getMoveType = function(){
+            showMoveAnimation();
+            
+            if (currentCPUMove.type == "Attack"){
+                setTimeout(attackingMove, 1500);
+            } else {
+                setTimeout(defensiveMove, 1500);
+            }
+        };
+        
+        var showMoveAnimation = function(){
+            $("#attack-img").addClass("cpu-attack-img");
+            $("#attack-img").removeClass("hide");
+            $("#attack-img").fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100);
+        }
+        
+        setUpCPUField();
+    }
+};
+
+var playerTurn = {
+    play: function() {
+        
+    }
+};
+
+var loop = function() {
+    if (cpuPokemon.health <= 0 || userPokemon.health <= 0){
+        $("#game-over").removeClass("hide");
+        console.log("Game Over");
+    } else {
+        currentState.play();
+    }
+};
+
+var init = function() {
+    cpuPokemon = pikachu;
+    userPokemon = charmander;
+    $("#cpu-name").text(cpuPokemon.name);
+    $("#cpu-lvl").text("lvl " + cpuPokemon.lvl);
+    $("#user-name").text(userPokemon.name);
+    $("#user-lvl").text("lvl " + userPokemon.lvl);
+    currentState = cpuTurn;
+    loop();
+};
+
+
+
+init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
